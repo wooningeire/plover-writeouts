bb41b449b262c5c7bb1c3399a058b96f1fd534a8
wooningeire <22846982+wooningeire@users.noreply.github.com>
2024-07-03 02:13:55 -0400

    feat: vowel elision

--- plover_writeouts/WriteoutsDictionary.py
+++ plover_writeouts/WriteoutsDictionary.py
@@ -7,52 +7,16 @@ import plover.log
 
 from plover_writeouts.lib.DagTrie import DagTrie
 
-def get_outline_phonemes(outline: tuple[Stroke, ...]):
-    _CONSONANTS_CHORDS = {
-        Stroke.from_steno(steno_right): Stroke.from_steno(steno_left)
-        for steno_right, steno_left in {
-            "-F": "TP",
-            "-FB": "SR",
-            "-FL": "TPHR",
-            "-R": "R",
-            "-P": "P",
-            "-PB": "TPH",
-            "-PL": "PH",
-            "-B": "PW",
-            "-BG": "K",
-            "-L": "HR",
-            "-G": "TKPW",
-            "-T": "T",
-            "-S": "S",
-            "-D": "TK",
-            "-Z": "STKPW",
-        }.items()
-    }
+def get_stroke_phonemes(stroke: Stroke):
     _LEFT_BANK_CONSONANTS_SUBSTROKE = Stroke.from_steno("STKPWHR")
     _VOWELS_SUBSTROKE = Stroke.from_steno("AOEU")
     _RIGHT_BANK_CONSONANTS_SUBSTROKE = Stroke.from_steno("-FRPBLGTSDZ")
 
+    left_bank_consonants = stroke & _LEFT_BANK_CONSONANTS_SUBSTROKE
+    vowels = stroke & _VOWELS_SUBSTROKE
+    right_bank_consonants = stroke & _RIGHT_BANK_CONSONANTS_SUBSTROKE
 
-    consonant_phonemes: list[Stroke] = []
-    phonemes: list[Stroke] = []
-    for stroke in outline:
-        left_bank_consonant_keys = (stroke & _LEFT_BANK_CONSONANTS_SUBSTROKE)
-        vowel_keys = (stroke & _VOWELS_SUBSTROKE)
-        right_bank_consonant_keys = (stroke & _RIGHT_BANK_CONSONANTS_SUBSTROKE)
-
-
-        phonemes.append(left_bank_consonant_keys)
-        consonant_phonemes.append(left_bank_consonant_keys)
-
-        phonemes.append(vowel_keys)
-
-        if right_bank_consonant_keys in _CONSONANTS_CHORDS:
-            mapped_keys = _CONSONANTS_CHORDS[right_bank_consonant_keys]
-
-            phonemes.append(mapped_keys)
-            consonant_phonemes.append(mapped_keys)
-
-    return tuple(phonemes), tuple(consonant_phonemes)
+    return left_bank_consonants, vowels, right_bank_consonants
 
 
 class WriteoutsDictionary(StenoDictionary):
@@ -73,13 +37,91 @@ class WriteoutsDictionary(StenoDictionary):
 
         self.__dag_trie = dag_trie = DagTrie()
 
+        _CHORD_ALTERNATIVES: dict[Stroke, Stroke] = {
+            Stroke.from_steno(steno_main): Stroke.from_steno(steno_alt)
+            for steno_main, steno_alt in {
+                "-F": "TP",
+                "-FB": "SR",
+                "-FL": "TPHR",
+                "-R": "R",
+                "-P": "P",
+                "-PB": "TPH",
+                "-PL": "PH",
+                "-B": "PW",
+                "-BG": "K",
+                "-L": "HR",
+                "-G": "TKPW",
+                "-T": "T",
+                "-S": "S",
+                "-D": "TK",
+                "-Z": "STKPW",
+                
+                "TP": "-F",
+                "SR": "-FB",
+                "TPHR": "-FL",
+                "R": "-R",
+                "P": "-P",
+                "TPH": "-PB",
+                "PH": "-PL",
+                "PW": "-B",
+                "K": "-BG",
+                "HR": "-L",
+                "TKPW": "-G",
+                "T": "-T",
+                "S": "-S",
+                "TK": "-D",
+                "STKPW": "-Z",
+            }.items()
+        }
+
         for outline_steno, translation in map.items():
             current_head = DagTrie.ROOT
 
+            last_prevowels_node = None
+            consonants_since_last_vowel = []
+
             for stroke_steno in outline_steno.split("/"):
                 stroke = Stroke.from_steno(stroke_steno)
-                for key in stroke.keys():
-                    current_head = dag_trie.get_dest_node(current_head, Stroke.from_steno(key))
+
+                left_bank_consonants, vowels, right_bank_consonants = get_stroke_phonemes(stroke)
+
+                preleft_node = current_head
+                if len(left_bank_consonants) > 0:
+                    left_bank_consonants_keys = tuple(Stroke.from_keys((key,)) for key in left_bank_consonants.keys())
+
+                    current_head = dag_trie.get_dst_node_else_create_chain(current_head, left_bank_consonants_keys)
+                    if left_bank_consonants in _CHORD_ALTERNATIVES:
+                        alternative_keys = tuple(Stroke.from_keys((key,)) for key in _CHORD_ALTERNATIVES[left_bank_consonants].keys())
+                        dag_trie.link_chain(preleft_node, current_head, alternative_keys)
+
+                    if last_prevowels_node is not None:
+                        dag_trie.link_chain(last_prevowels_node, current_head, left_bank_consonants_keys)
+
+                    if left_bank_consonants in _CHORD_ALTERNATIVES and last_prevowels_node is not None:
+                        dag_trie.link_chain(last_prevowels_node, current_head, alternative_keys)
+
+                    last_prevowels_node = current_head
+
+                if len(vowels) > 0:
+                    current_head = dag_trie.get_dst_node_else_create(current_head, vowels)
+
+                if len(right_bank_consonants) > 0:
+                    right_bank_consonants_keys = tuple(Stroke.from_keys((key,)) for key in right_bank_consonants.keys())
+
+                    current_head = dag_trie.get_dst_node_else_create_chain(current_head, right_bank_consonants_keys)
+                    if right_bank_consonants_keys in _CHORD_ALTERNATIVES:
+                        alternative_keys = tuple(Stroke.from_keys((key,)) for key in _CHORD_ALTERNATIVES[right_bank_consonants].keys())
+                        dag_trie.link_chain(preleft_node, current_head, alternative_keys)
+
+                    if last_prevowels_node is not None:
+                        dag_trie.link_chain(last_prevowels_node, current_head, right_bank_consonants_keys)
+
+                    if left_bank_consonants in _CHORD_ALTERNATIVES and last_prevowels_node is not None:
+                        dag_trie.link_chain(last_prevowels_node, current_head, alternative_keys)
+
+                    last_prevowels_node = current_head
+
+
 
             dag_trie.set_translation(current_head, translation)
 
@@ -106,11 +148,31 @@ class WriteoutsDictionary(StenoDictionary):
 
         dag_trie = self.__dag_trie
 
-        for stroke_steno in stroke_stenos:
+        for stroke_steno in enumerate(stroke_stenos):
             stroke = Stroke.from_steno(stroke_steno)
-            for key in stroke.keys():
-                current_head = dag_trie.find_dest_node(current_head, Stroke.from_steno(key))
+            if len(stroke) == 0:
+                return None
 
+            left_bank_consonants, vowels, right_bank_consonants = get_stroke_phonemes(stroke)
+
+            if len(left_bank_consonants) > 0:
+                left_bank_consonants_keys = tuple(Stroke.from_keys((key,)) for key in left_bank_consonants.keys())
+                current_head = dag_trie.get_dst_node_chain(current_head, left_bank_consonants_keys)
+                plover.log.info(left_bank_consonants_keys)
+                if current_head is None:
+                    return None
+
+            if len(vowels) == 0:
+                return None
+            current_head = dag_trie.get_dst_node(current_head, vowels)
+            plover.log.info(vowels)
+            if current_head is None:
+                return None
+
+            if len(right_bank_consonants) > 0:
+                right_bank_consonants_keys = tuple(Stroke.from_keys((key,)) for key in right_bank_consonants.keys())
+                current_head = dag_trie.get_dst_node_chain(current_head, right_bank_consonants_keys)
+                plover.log.info(right_bank_consonants_keys)
                 if current_head is None:
                     return None
 
--- plover_writeouts/lib/DagTrie.py
+++ plover_writeouts/lib/DagTrie.py
@@ -9,8 +9,8 @@ class DagTrie:
         self.__nodes: list[dict[Stroke, int]] = [{}]
         self.__words: dict[int, str] = {}
 
-    def get_dest_node(self, from_node: int, key: Stroke):
-        transitions = self.__nodes[from_node]
+    def get_dst_node_else_create(self, src_node: int, key: Stroke):
+        transitions = self.__nodes[src_node]
         if key in transitions:
             return transitions[key]
         
@@ -19,9 +19,32 @@ class DagTrie:
         self.__nodes.append({})
 
         return new_node_id
+
+    def get_dst_node_else_create_chain(self, src_node: int, strokes: tuple[Stroke, ...]):
+        current_node = src_node
+        for stroke in strokes:
+            current_node = self.get_dst_node_else_create(current_node, stroke)
+        return current_node
     
-    def find_dest_node(self, from_node: int, key: Stroke):
-        return self.__nodes[from_node].get(key, None)
+    def get_dst_node(self, src_node: int, key: Stroke):
+        return self.__nodes[src_node].get(key, None)
+    
+    def get_dst_node_chain(self, src_node: int, strokes: tuple[Stroke, ...]):
+        current_node = src_node
+        for stroke in strokes:
+            current_node = self.get_dst_node(current_node, stroke)
+            if current_node is None:
+                return None
+        return current_node
+    
+    def link(self, src_node: int, dst_node: int, key: Stroke):
+        self.__nodes[src_node][key] = dst_node
+    
+    def link_chain(self, src_node: int, dst_node: int, strokes: tuple[Stroke, ...]):
+        current_node = src_node
+        for stroke in strokes[:-1]:
+            current_node = self.get_dst_node_else_create(current_node, stroke)
+        self.__nodes[current_node][strokes[-1]] = dst_node
     
     def set_translation(self, node: int, translation: str):
         self.__words[node] = translation
