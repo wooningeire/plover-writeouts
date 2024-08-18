70bfec95e6060c1367c68072af935608fa1e67cd
wooningeire <22846982+wooningeire@users.noreply.github.com>
2024-08-18 05:08:02 -0400

    feat: asterisk support in chord matching

--- plover_writeouts/lib/intermediate.py
+++ plover_writeouts/lib/intermediate.py
@@ -1,22 +1,34 @@
 from dataclasses import dataclass
-from typing import cast
+from typing import Iterable, cast
 
 from plover.steno import Stroke
 
-from .util import can_add_stroke_on, split_stroke_parts
+from .util import can_add_stroke_on
 
-_VOWELS = set("aeiou")
+_ASTERISK_SUBSTROKE = Stroke.from_steno("*")
+    
+@dataclass(frozen=True)
+class _AnnotatedKey:
+    key: str
+    asterisk: bool
+
+    @staticmethod
+    def annotations_from_outline(outline_steno: str):
+        return tuple(
+            _AnnotatedKey(key, has_asterisk)
+            for stroke, has_asterisk in (
+                (stroke - _ASTERISK_SUBSTROKE, _ASTERISK_SUBSTROKE in stroke)
+                for stroke in (
+                    Stroke.from_steno(steno)
+                    for steno in outline_steno.split("/")
+                )
+            )
+            for key in stroke.keys() 
+        )
 
 _GRAPHEME_TO_STENO_MAPPINGS = {
     grapheme: sorted(
-        tuple(
-            tuple(
-                key
-                for steno in outline_steno.split("/")
-                for key in Stroke.from_steno(steno).keys() 
-            )
-            for outline_steno in outline_stenos
-        ),
+        tuple(_AnnotatedKey.annotations_from_outline(outline_steno) for outline_steno in outline_stenos),
         key=lambda keys: len(keys), reverse=True
     )
     for grapheme, outline_stenos in cast(dict[str, tuple[str, ...]], {
@@ -40,29 +52,54 @@ _GRAPHEME_TO_STENO_MAPPINGS = {
         "n": ("TPH", "-PB"),
         "o": ("O", "OE", "O/W", "OE/W"),
         "p": ("P", "-P"),
-        "q": ("KW",),
+        "q": ("K", "-BG"),
         "r": ("R", "-R"),
-        "s": ("S", "-S"),
+        "s": ("S", "-S", "-F", "-Z"),
         "t": ("T", "-T"),
         "u": ("U", "AOU", "U/W", "AOU/W"),
         "v": ("SR", "-F"),
         "w": ("W", "U"),
         "x": ("KP", "-BGS", "-BG/S"),
-        "y": ("KWH", "EU"),
+        "y": ("KWH", "EU", "AOEU", "EU/KWR", "AOEU/KWR"),
         "z": ("STKPW", "-Z"),
 
-        "dg": ("SKWR", "-PBLG"),
+        "th": ("TH", "*T"),
+        "sh": ("SH", "-RB"),
+        "ch": ("KH", "-FP"),
+
         "oo": ("AO",),
         "ou": ("U",),
         "ea": ("AOE", "AE"),
-        "ti": ("SH", "-RB/KWR"),
-        "ci": ("SH", "-RB/KWR"),
+        "ae": ("AE", "AEU"),
+        "dg": ("SKWR", "-PBLG"),
+        "ck": ("K", "-BG"),
+        "ti": ("SH", "-RB", "-RB/KWR"),
+        "ci": ("SH", "-RB", "-RB/KWR"),
+        "mp": ("*PL",),
+
+        "bb": ("PW", "-B"),
+        "cc": ("S", "K", "KR", "-BG", "-S"),
+        "dd": ("TK", "-D"),
+        "ff": ("TP", "-F"),
+        "gg": ("SKWR", "TKPW", "-PBLG", "-G"),
+        "jj": ("SKWR", "-PBLG", "-G"),
+        "kk": ("K", "-BG", "*G"),
+        "ll": ("HR", "-L"),
+        "mm": ("PH", "-PL"),
+        "nn": ("TPH", "-PB"),
+        "pp": ("P", "-P"),
+        "rr": ("R", "-R"),
+        "ss": ("S", "-S"),
+        "tt": ("T", "-T"),
+        "vv": ("SR", "-F"),
+        "xx": ("KP", "-BGS", "-BG/S"),
+        "zz": ("STKPW", "-Z"),
     }).items()
 }
 """A list of what counts as a "match" when matching characters to keys."""
 
 @dataclass(frozen=True)
-class Cell:
+class _Cell:
     """A cell in the Needleman–Wunsch alignment matrix; represents an optimal alignment of the first x characters in a translation to the first y keys in an outline."""
 
     n_unmatched_chars: int
@@ -75,13 +112,14 @@ class Cell:
     unmatched_key_start_index: int
     """The index where the sequence of trailing unmatched keys for this alignment beigins. This specifies which keys to check when trying to find matches."""
     
-    parent: "Cell | None"
+    parent: "_Cell | None"
     """The optimal sub-alignment which this alignment extends upon."""
 
     x: int
     y: int
 
     has_match: bool
+    asterisk_matches: tuple[bool, ...] = ()
 
     @property
     def cost(self):
@@ -99,12 +137,15 @@ class Lexeme:
     __repr__ = __str__
 
     @staticmethod
-    def keys_to_strokes(keys: tuple[str, ...]):
+    def keys_to_strokes(keys: Iterable[str], asterisk_matches: Iterable[bool]):
         strokes: list[Stroke] = []
 
         current_stroke = Stroke.from_integer(0)
-        for key in keys:
+        for key, asterisk_match in zip(keys, asterisk_matches):
             key_stroke = Stroke.from_keys((key,))
+            if asterisk_match:
+                key_stroke += _ASTERISK_SUBSTROKE
+
             if can_add_stroke_on(current_stroke, key_stroke):
                 current_stroke += key_stroke
             else:
@@ -125,22 +166,18 @@ def match_graphemes_to_writeout_chords(translation: str, outline_steno: str):
     - Strict left-to-right parsing; no inversions
     """
 
-    keys = tuple(
-        key
-        for stroke_steno in outline_steno.split("/")
-        for key in Stroke.from_steno(stroke_steno).keys()
-    )
+    annotated_keys = _AnnotatedKey.annotations_from_outline(outline_steno)
 
 
     # Base row and column
 
-    matrix = [[Cell(0, 0, 0, 0, None, 0, 0, False)]]
+    matrix = [[_Cell(0, 0, 0, 0, None, 0, 0, False)]]
 
     for i in range(len(translation)):
-        matrix.append([Cell(i + 1, 0, 0, 0, matrix[-1][0], i + 1, 0, False)])
+        matrix.append([_Cell(i + 1, 0, 0, 0, matrix[-1][0], i + 1, 0, False)])
 
-    for i in range(len(keys)):
-        matrix[0].append(Cell(0, i + 1, 0, 0, matrix[0][-1], 0, i + 1, False))
+    for i in range(len(annotated_keys)):
+        matrix[0].append(_Cell(0, i + 1, 0, 0, matrix[0][-1], 0, i + 1, False))
 
 
     # Populating the matrix
@@ -154,22 +191,28 @@ def match_graphemes_to_writeout_chords(translation: str, outline_steno: str):
         base = matrix[x][y]
 
         candidate_chars = translation[:x + 1]
-        candidate_keys = keys[:y + 1]
+        candidate_keys = annotated_keys[:y + 1]
 
         # l()
         # l(unmatched_chars, unmatched_keys, x, y)
 
-        for i in reversed(range(len(candidate_chars) + 1)):
+        for i in reversed(range(1, len(candidate_chars) + 1)):
             grapheme = candidate_chars[-i:]
             # l("using grapheme", grapheme)
             if grapheme not in _GRAPHEME_TO_STENO_MAPPINGS: continue
 
             for chord in _GRAPHEME_TO_STENO_MAPPINGS[grapheme]:
                 # l("testing chord", chord)
-                if candidate_keys[-len(chord):] != chord: continue
+                sub_candidate_keys = candidate_keys[-len(chord):]
+                if tuple(key.key for key in sub_candidate_keys) != tuple(key.key for key in chord): continue
+
+                if any(
+                    chord_key.asterisk and not candidate_key.asterisk
+                    for candidate_key, chord_key in zip(sub_candidate_keys, chord)
+                ): continue
                 
                 # l("found", grapheme, chord)
-                return Cell(
+                return _Cell(
                     base.n_unmatched_chars + 1 - len(grapheme),
                     base.n_unmatched_keys + 1 - len(chord),
                     x + 1,
@@ -178,10 +221,11 @@ def match_graphemes_to_writeout_chords(translation: str, outline_steno: str):
                     x + 1,
                     y + 1,
                     True,
+                    tuple(key.asterisk for key in chord),
                 )
 
         # l("not found")
-        return Cell(
+        return _Cell(
             base.n_unmatched_chars + 1,
             base.n_unmatched_keys + 1,
             base.unmatched_char_start_index,
@@ -194,10 +238,10 @@ def match_graphemes_to_writeout_chords(translation: str, outline_steno: str):
     
     
     for x in range(len(translation)):
-        for y in range(len(keys)):
+        for y in range(len(annotated_keys)):
             # Increment x: add a character from the translation
             x_base = matrix[x][y + 1]
-            x_candidate = Cell(
+            x_candidate = _Cell(
                 x_base.n_unmatched_chars + 1,
                 x_base.n_unmatched_keys,
                 x_base.unmatched_char_start_index,
@@ -210,7 +254,7 @@ def match_graphemes_to_writeout_chords(translation: str, outline_steno: str):
 
             # Increment y: add a key from the outline
             y_base = matrix[x + 1][y]
-            y_candidate = Cell(
+            y_candidate = _Cell(
                 y_base.n_unmatched_chars,
                 y_base.n_unmatched_keys + 1,
                 y_base.unmatched_char_start_index,
@@ -247,12 +291,14 @@ def match_graphemes_to_writeout_chords(translation: str, outline_steno: str):
     while current_cell.parent is not None:
         if current_cell.has_match:
             start_cell = current_cell.parent
+            asterisk_matches = current_cell.asterisk_matches
         else:
             start_cell = matrix[current_cell.parent.unmatched_char_start_index][current_cell.parent.unmatched_key_start_index]
+            asterisk_matches = (False,) * (current_cell.y - start_cell.y)
 
         lexemes.append(Lexeme(
             ortho=translation[start_cell.x:current_cell.x],
-            steno=Lexeme.keys_to_strokes(keys[start_cell.y:current_cell.y]),
+            steno=Lexeme.keys_to_strokes((key.key for key in annotated_keys[start_cell.y:current_cell.y]), asterisk_matches),
             phono="",
         ))
 
--- test/test_intermediate.py
+++ test/test_intermediate.py
@@ -22,10 +22,18 @@ def test__match_graphemes_to_writeout_chords__silent_chords():
         == "(p.P, u.AOU, p.-P, .KWR, a.A, l.-L)"
     )
 
+def test__match_graphemes_to_writeout_chords__asterisk_chords():
+    from plover_writeouts.lib.intermediate import match_graphemes_to_writeout_chords
+
+    assert (
+        str(match_graphemes_to_writeout_chords("zenith", "STKPWE/TPH*EUT"))
+        == "(z.STKPW, e.E, n.TPH, i.EU, th.*T)"
+    )
+
 def test__match_graphemes_to_writeout_chords__length_stress_test():
     from plover_writeouts.lib.intermediate import match_graphemes_to_writeout_chords
 
     assert (
         str(match_graphemes_to_writeout_chords("supercalifragilisticexpialidocious", "SAOU/PER/KA/HREU/TPRA/SKWREU/HREU/STEUS/KWREBGS/PEU/KWRA/HREU/TKOERB/KWRUS"))
-        == "(s.S, u.AOU, p.P, e.E, r.-R, c.K, a.A, l.HR, i.EU, f.TP, r.R, a.A, g.SKWR, i.EU, l.HR, i.EU, s.S, t.T, i.EU, c.-S, .KWR, e.E, x.-BGS, p.P, i.EU/KWR, a.A, l.HR, i.EU, d.TK, o.OE, ci.-RB, .KWR, ou.U, s.-S)"
+        == "(s.S, u.AOU, p.P, e.E, r.-R, c.K, a.A, l.HR, i.EU, f.TP, r.R, a.A, g.SKWR, i.EU, l.HR, i.EU, s.S, t.T, i.EU, c.-S, .KWR, e.E, x.-BGS, p.P, i.EU/KWR, a.A, l.HR, i.EU, d.TK, o.OE, ci.-RB/KWR, ou.U, s.-S)"
     )
